463756d8ed92ecfb2329258ade1b7de3b9eba852
Brian Wangora <brianwangora@gmail.com>
2022-06-03 13:57:39 +0300

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts
+}
+*/
+/* 
+for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+  debugger;
+}
+*/
+
+const message =[];
+function writeCards(name, event ) {
+    for (let i=0; i<name.length; i++) {
+        message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return message
+} 
+
+
+function countDown () {
+    let countDown = 10    
+    while (countDown>=0) {
+    console.log(countDown--);
+    }
+}
